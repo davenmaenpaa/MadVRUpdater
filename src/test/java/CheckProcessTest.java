d8c36943708f03ba257ae510ace1539cdffc3ce5
Daven Mäenpää <daven.maenpaa@outlook.com>
2017-12-11 16:35:06 +0100

    test for checkprocess

---
+++ src/test/java/CheckProcessTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckProcessTest {
+
+    @Test
+    public void checkProcess() {
+        assertEquals(true, CheckProcess.checkProcess());
+    }
+}
